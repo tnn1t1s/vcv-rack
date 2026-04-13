5ec5f92ee09f58c07a60134e1249abdedf1112de
tnn1t1s <david.j.palaitis@gmail.com>
2026-04-13 15:26:48 -0400

    Remove Inspector module

--- plugin/AgentRack/src/plugin.cpp
+++ plugin/AgentRack/src/plugin.cpp
@@ -3,12 +3,17 @@
 // Forward declarations -- one per module source file
 extern rack::Model* modelAttenuate;
 extern rack::Model* modelADSR;
+extern rack::Model* modelCassette;
 extern rack::Model* modelCrinkle;
-extern rack::Model* modelInspector;
 extern rack::Model* modelLadder;
 extern rack::Model* modelNoise;
 extern rack::Model* modelSaphire;
 extern rack::Model* modelSonic;
+extern rack::Model* modelSteel;
+extern rack::Model* modelBusCrush;
+extern rack::Model* modelClockDiv;
+extern rack::Model* modelTonnetz;
+extern rack::Model* modelMaurizio;
 
 rack::Plugin* pluginInstance;
 
@@ -16,10 +21,15 @@ void init(rack::Plugin* p) {
     pluginInstance = p;
     p->addModel(modelAttenuate);
     p->addModel(modelADSR);
+    p->addModel(modelCassette);
     p->addModel(modelCrinkle);
-    p->addModel(modelInspector);
     p->addModel(modelLadder);
     p->addModel(modelNoise);
     p->addModel(modelSaphire);
     p->addModel(modelSonic);
+    p->addModel(modelSteel);
+    p->addModel(modelBusCrush);
+    p->addModel(modelClockDiv);
+    p->addModel(modelTonnetz);
+    p->addModel(modelMaurizio);
 }
